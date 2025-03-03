471d1f8b344d76f16e20145a2f4a0e6a365d8274
mcmul <mcmulle20.laxwi@gmail.com>
2025-03-02 22:44:35 -0600

    render debuffing

--- server/dist/routes/index.js
+++ server/dist/routes/index.js
@@ -4,6 +4,8 @@ import apiRoutes from './api/index.js';
 import { authenticateToken } from '../middleware/auth.js';
 const router = Router();
 router.use('/auth', authRoutes);
+// Log to confirm the routes are being set up
+console.log('Setting up routes');
 // TODO: Add authentication to the API routes
 router.use('/api', authenticateToken, apiRoutes);
 export default router;
